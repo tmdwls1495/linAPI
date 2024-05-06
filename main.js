b15b66f2452fe5f97a0b037ee089dc402dde2391
inseungjin <tmdwls1495@naver.com>
2024-05-07 02:09:34 +0900

    four

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const API_KEY =  ``
+const API_KEY =  `ce9e75c4c7814be1ac886bbf0386c36c`;
+
+let news = []
+const getLateNews = async() =>{
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const response = await fetch(url);
+    const data = await response.json() //json = 파일 확장자 중 하나, 객체 형식처럼 사용//
+    news = data.articles;
+    console.log("dddd", news); 
+};
+
+getLateNews();
